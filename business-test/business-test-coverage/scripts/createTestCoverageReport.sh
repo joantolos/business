439d105a94c0f509e78de24a0b22f3f5ff1de26a
jtolos <Mrfalken3>
2015-04-27 17:58:34 +0200

    Package refactor

--- business-test/business-test-coverage/scripts/createTestCoverageReport.sh
+++ business-test/business-test-coverage/scripts/createTestCoverageReport.sh
@@ -3,22 +3,17 @@
 cd .. &&
 cd .. &&
 cd .. &&
-
 #Cleaning the whole project
 mvn clean compile &&
-
 #Instrument Cobertura
 cd business-test/business-test-coverage/scripts &&
 ant instrument &&
-
 #Going to parent's folder
 cd .. &&
 cd .. &&
 cd .. &&
-
 #Generating tests classes
 mvn test &&
-
 #Creating the Cobertura Report and cleaning Cobertura files
 cd business-test/business-test-coverage/scripts &&
 ant report &&
